0929a8a6d7106313eb938106de9ec986d9785185
Tothster <itstothster@gmail.com>
2023-03-12 12:36:32 -0300

    fixing mobile again

--- packages/ui/overlay/Content.tsx
+++ packages/ui/overlay/Content.tsx
@@ -9,7 +9,7 @@ export interface Content {
 
 export const Content = forwardRef<HTMLDivElement, Content>(({ className, children }, ref) => {
   return (
-    <div ref={ref} className={classNames(className, 'inline-block w-full pt-12 pb-[68px] !my-0 h-full px-3')}>
+    <div ref={ref} className={classNames(className, 'inline-block w-full pt-12 pb-[68px] px-3')}>
       {children}
     </div>
   )
