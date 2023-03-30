64cb73b426f573c43dcc6dbc45cae248e3192fc1
tothster <moura.roberto@eb.mil.br>
2023-03-30 15:31:41 -0300

    test hoursnap

--- apps/earn/pages/api/hoursnap.ts
+++ apps/earn/pages/api/hoursnap.ts
@@ -3,18 +3,29 @@ import { prisma } from '@dozer/database'
 
 export default async function handler(request: NextApiRequest, response: NextApiResponse) {
   // if (request.query.key && request.query.key === process.env.API_KEY) {
-  const pools = await prisma.pool.findMany()
-  pools.forEach(async (pool) => {
-    const snap = await prisma.hourSnapshot.create({
-      data: {
-        poolId: pool.id,
-        apr: pool.apr,
-        date: new Date(),
-        liquidityUSD: pool.liquidityUSD,
-        volumeUSD: pool.volumeUSD,
-      },
-    })
+  // const pools = await prisma.pool.findMany()
+  // let snaps = []
+  // pools.forEach(async (pool) => {
+  //   const snap = await prisma.hourSnapshot.create({
+  //     data: {
+  //       poolId: pool.id,
+  //       apr: pool.apr,
+  //       date: new Date(),
+  //       liquidityUSD: pool.liquidityUSD,
+  //       volumeUSD: pool.volumeUSD,
+  //     },
+  //   })
+  //   snaps.push(snap)
+  // })
+  const snap = await prisma.hourSnapshot.create({
+    data: {
+      poolId: '6',
+      apr: 10,
+      date: new Date(),
+      liquidityUSD: 100,
+      volumeUSD: 1000,
+    },
   })
-  return response.status(200).end(`Updated !`)
+  return response.status(200).end(snap)
   // } else return response.status(401).end(`Not Authorized !`)
 }
