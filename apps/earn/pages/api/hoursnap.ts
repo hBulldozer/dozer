59bfe433eb6065a800c56e3a5673b9f23813d48b
tothster <moura.roberto@eb.mil.br>
2023-03-30 15:57:05 -0300

    fix hoursnaṕ 2

--- apps/earn/pages/api/hoursnap.ts
+++ apps/earn/pages/api/hoursnap.ts
@@ -2,30 +2,29 @@ import { NextApiRequest, NextApiResponse } from 'next'
 import { prisma } from '@dozer/database'
 
 export default async function handler(request: NextApiRequest, response: NextApiResponse) {
-  // if (request.query.key && request.query.key === process.env.API_KEY) {
+  if (request.query.key && request.query.key === process.env.API_KEY) {
   // const pools = await prisma.pool.findMany()
-  // let snaps = []
+  const pool = await prisma.pool.findFirst()
   // pools.forEach(async (pool) => {
-  //   const snap = await prisma.hourSnapshot.create({
-  //     data: {
-  //       poolId: pool.id,
-  //       apr: pool.apr,
-  //       date: new Date(),
-  //       liquidityUSD: pool.liquidityUSD,
-  //       volumeUSD: pool.volumeUSD,
-  //     },
-  //   })
-  //   snaps.push(snap)
+    const snap = await prisma.hourSnapshot.create({
+      data: {
+        poolId: pool ? pool.id : '0',
+        apr: pool? pool.apr:0,
+        date: new Date(),
+        liquidityUSD: pool? pool.liquidityUSD: 10,
+        volumeUSD: pool? pool.volumeUSD: 100,
+      },
+    })
   // })
-  const snap = await prisma.hourSnapshot.create({
-    data: {
-      poolId: '0',
-      apr: 10,
-      date: new Date(),
-      liquidityUSD: 100,
-      volumeUSD: 1000,
-    },
-  })
+  // const snap = await prisma.hourSnapshot.create({
+  //   data: {
+  //     poolId: '0',
+  //     apr: 10,
+  //     date: new Date(),
+  //     liquidityUSD: 100,
+  //     volumeUSD: 1000,
+  //   },
+  // })
   return response.status(200).end('snap ' + snap.id + ' criado')
   // } else return response.status(401).end(`Not Authorized !`)
 }
