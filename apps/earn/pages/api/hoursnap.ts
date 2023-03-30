90698b7f024574cda6deee9d13c30857654e5b96
tothster <moura.roberto@eb.mil.br>
2023-03-30 15:17:20 -0300

    endpoint without protection

--- apps/earn/pages/api/hoursnap.ts
+++ apps/earn/pages/api/hoursnap.ts
@@ -2,19 +2,19 @@ import { NextApiRequest, NextApiResponse } from 'next'
 import { prisma } from '@dozer/database'
 
 export default async function handler(request: NextApiRequest, response: NextApiResponse) {
-  if (request.query.key && request.query.key === process.env.API_KEY) {
-    const pools = await prisma.pool.findMany()
-    pools.forEach(async (pool) => {
-      const snap = await prisma.hourSnapshot.create({
-        data: {
-          poolId: pool.id,
-          apr: pool.apr,
-          date: new Date(),
-          liquidityUSD: pool.liquidityUSD,
-          volumeUSD: pool.volumeUSD,
-        },
-      })
+  // if (request.query.key && request.query.key === process.env.API_KEY) {
+  const pools = await prisma.pool.findMany()
+  pools.forEach(async (pool) => {
+    const snap = await prisma.hourSnapshot.create({
+      data: {
+        poolId: pool.id,
+        apr: pool.apr,
+        date: new Date(),
+        liquidityUSD: pool.liquidityUSD,
+        volumeUSD: pool.volumeUSD,
+      },
     })
-    return response.status(200).end(`Updated !`)
-  } else return response.status(401).end(`Not Authorized !`)
+  })
+  return response.status(200).end(`Updated !`)
+  // } else return response.status(401).end(`Not Authorized !`)
 }
