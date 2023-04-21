595f8e966de13647038d34340104422237afbcb4
Tothster <itstothster@gmail.com>
2023-04-21 13:47:42 -0300

    testing getstaticprops

--- apps/earn/pages/[id]/add.tsx
+++ apps/earn/pages/[id]/add.tsx
@@ -2,7 +2,7 @@ import { ExternalLinkIcon } from '@heroicons/react/solid'
 import { ChainId, chainName } from '@dozer/chain'
 import { formatPercent } from '@dozer/format'
 // import { getBuiltGraphSDK, Pair } from '@dozer/graph-client'
-import { Pair, pairFromPoolAndTokens } from '../../utils/Pair'
+import { Pair, pairFromPoolAndTokens, pairFromPoolAndTokensList } from '../../utils/Pair'
 import { AppearOnMount, BreadcrumbLink, Container, Link, Typography } from '@dozer/ui'
 // import { SUPPORTED_CHAIN_IDS } from '../../config'
 import {
@@ -15,7 +15,7 @@ import {
 } from 'next'
 import { useRouter } from 'next/router'
 import { FC } from 'react'
-// import useSWR, { SWRConfig } from 'swr'
+import useSWR, { SWRConfig } from 'swr'
 
 import {
   AddSectionLegacy,
@@ -67,21 +67,23 @@ const LINKS = ({ pair }: { pair: Pair }): BreadcrumbLink[] => [
 //   }
 // }
 
-// const Add: FC<InferGetStaticPropsType<typeof getStaticProps>> = ({ fallback }) => {
-//   return (
-//     // <SWRConfig value={{ fallback }}>
-//     <_Add />
-//     // </SWRConfig>
-//   )
-// }
+const Add: FC<InferGetStaticPropsType<typeof getStaticProps>> = ({ fallback }) => {
+  return (
+    <SWRConfig value={{ fallback }}>
+      <_Add />
+    </SWRConfig>
+  )
+}
 
-const Add: NextPage = ({ pair }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+const _Add: NextPage = () => {
   const router = useRouter()
-  // const { data } = useSWR<{ pair: Pair }>(`/earn/api/pool/${router.query.id}`, (url) =>
-  //   fetch(url).then((response) => response.json())
-  // )
+  const { data } = useSWR<{ pair: Pair }>(`/earn/api/pool/${router.query.id}`, (url) =>
+    fetch(url).then((response) => response.json())
+  )
 
-  // if (!data) return <></>
+  if (!data) return <></>
+
+  const { pair } = data
 
   return (
     // <PoolPositionProvider pair={pair}>
@@ -121,56 +123,48 @@ const Add: NextPage = ({ pair }: InferGetServerSidePropsType<typeof getServerSid
   )
 }
 
-// export const getStaticPaths: GetStaticPaths = async () => {
-//   // When this is true (in preview environments) don't
-//   // prerender any static pages
-//   // (faster builds, but slower initial page load)
-//   if (process.env.SKIP_BUILD_STATIC_GENERATION === 'true') {
-//     return {
-//       paths: [],
-//       fallback: 'blocking',
-//     }
-//   }
+export const getStaticPaths: GetStaticPaths = async () => {
+  const pre_pools = await prisma.pool.findMany()
+  const tokens = await prisma.token.findMany()
+  const pairs: Pair[] = []
+  pre_pools.forEach((pool) => {
+    pairs?.push(pairFromPoolAndTokensList(pool, tokens))
+  })
 
-//   const sdk = getBuiltGraphSDK()
-//   const { pairs } = await sdk.PairsByChainIds({
-//     first: 250,
-//     orderBy: 'liquidityUSD',
-//     orderDirection: 'desc',
-//     chainIds: SUPPORTED_CHAIN_IDS,
-//   })
+  // Get the paths we want to pre-render based on pairs
+  const paths = pairs.map((pair, i) => ({
+    params: { id: `${pair.id}` },
+  }))
 
-//   // Get the paths we want to pre-render based on pairs
-//   const paths = pairs
-//     .sort(({ liquidityUSD: a }, { liquidityUSD: b }) => {
-//       return Number(b) - Number(a)
-//     })
-//     .slice(0, 250)
-//     .map((pair, i) => ({
-//       params: { id: `${chainName[pair.chainId]}:${pair.address}` },
-//     }))
+  // We'll pre-render only these paths at build time.
+  // { fallback: blocking } will server-render pages
+  // on-demand if the path doesn't exist.
+  return { paths, fallback: 'blocking' }
+}
 
-//   // We'll pre-render only these paths at build time.
-//   // { fallback: blocking } will server-render pages
-//   // on-demand if the path doesn't exist.
-//   return { paths, fallback: 'blocking' }
-// }
-
-// export const getStaticProps: GetStaticProps = async ({ params }) => {
-//   const sdk = getBuiltGraphSDK()
-//   const id = params?.id as string
-//   const { pair } = await sdk.PairById({ id })
-//   if (!pair) {
-//     throw new Error(`Failed to fetch pair, received ${pair}`)
-//   }
-//   return {
-//     props: {
-//       fallback: {
-//         [`/earn/api/pool/${id}`]: { pair },
-//       },
-//     },
-//     revalidate: 60,
-//   }
-// }
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const id = params?.id as string
+  const pre_pool = await prisma.pool.findUnique({
+    where: { id: id },
+    include: {
+      token0: true,
+      token1: true,
+      hourSnapshots: { orderBy: { date: 'desc' } },
+      daySnapshots: { orderBy: { date: 'desc' } },
+    },
+  })
+  const pair: Pair = pairFromPoolAndTokens(pre_pool)
+  if (!pair) {
+    throw new Error(`Failed to fetch pair, received ${pair}`)
+  }
+  return {
+    props: {
+      fallback: {
+        [`/earn/api/pool/${id}`]: { pair },
+      },
+    },
+    revalidate: 60,
+  }
+}
 
 export default Add
---
+++ apps/earn/pages/api/pool/[id].ts
@@ -0,0 +1,77 @@
+import prisma from '@dozer/database'
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { Pair, pairFromPoolAndTokens } from '../../../utils/Pair'
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  res.setHeader('Cache-Control', 'public, s-maxage=10, stale-while-revalidate=59')
+  if (!req.query?.id) res.status(422)
+  const pre_pool = await prisma.pool.findUnique({
+    where: { id: req.query.id as string },
+    include: {
+      token0: true,
+      token1: true,
+      hourSnapshots: { orderBy: { date: 'desc' } },
+      daySnapshots: { orderBy: { date: 'desc' } },
+    },
+  })
+
+  const pair: Pair = pairFromPoolAndTokens(pre_pool)
+
+  const tokens = await prisma.token.findMany({
+    select: {
+      id: true,
+      name: true,
+      uuid: true,
+      symbol: true,
+      chainId: true,
+      decimals: true,
+      pools0: {
+        select: {
+          id: true,
+          reserve0: true,
+          reserve1: true,
+          token1: {
+            select: {
+              uuid: true,
+            },
+          },
+        },
+      },
+      pools1: {
+        select: {
+          id: true,
+          reserve0: true,
+          reserve1: true,
+          token0: {
+            select: {
+              uuid: true,
+            },
+          },
+        },
+      },
+    },
+  })
+
+  const resp = await fetch('https://api.kucoin.com/api/v1/prices?currencies=HTR')
+  const data = await resp.json()
+  const priceHTR = data.data.HTR
+  const prices: { [key: string]: number | undefined } = {}
+
+  tokens.forEach((token) => {
+    if (token.uuid == '00') prices[token.uuid] = Number(priceHTR)
+    else if (token.pools0.length > 0) {
+      const poolHTR = token.pools0.find((pool) => {
+        return pool.token1.uuid == '00'
+      })
+      if (!prices[token.uuid]) prices[token.uuid] = (Number(poolHTR?.reserve1) / Number(poolHTR?.reserve0)) * priceHTR
+    } else if (token.pools1.length > 0) {
+      const poolHTR = token.pools1.find((pool) => {
+        return pool.token0.uuid == '00'
+      })
+      if (!prices[token.uuid]) prices[token.uuid] = (Number(poolHTR?.reserve0) / Number(poolHTR?.reserve1)) * priceHTR
+    }
+  })
+
+  // const [pair] = await Promise.all([getPool(req.query.id as string)])
+  res.status(200).send({ pair, prices })
+}
