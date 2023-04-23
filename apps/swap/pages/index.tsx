a4238548a1da16d17f9b7136e1810da463c603fc
Tothster <itstothster@gmail.com>
2023-04-23 16:00:04 -0300

    fix for build

--- apps/swap/pages/index.tsx
+++ apps/swap/pages/index.tsx
@@ -80,15 +80,15 @@ export const getStaticProps: GetStaticProps = async (context) => {
   const priceHTR = data.data.HTR
   const prices: { [key: string]: number | undefined } = {}
 
-  tokens.forEach((token: dbTokenWithPools) => {
+  tokens.forEach((token) => {
     if (token.uuid == '00') prices[token.uuid] = Number(priceHTR)
     else if (token.pools0.length > 0) {
-      const poolHTR = token.pools0.find((pool: dbPoolWithTokens) => {
+      const poolHTR = token.pools0.find((pool) => {
         return pool.token1.uuid == '00'
       })
       if (!prices[token.uuid]) prices[token.uuid] = (Number(poolHTR?.reserve1) / Number(poolHTR?.reserve0)) * priceHTR
     } else if (token.pools1.length > 0) {
-      const poolHTR = token.pools1.find((pool: dbPoolWithTokens) => {
+      const poolHTR = token.pools1.find((pool) => {
         return pool.token0.uuid == '00'
       })
       if (!prices[token.uuid]) prices[token.uuid] = (Number(poolHTR?.reserve0) / Number(poolHTR?.reserve1)) * priceHTR
