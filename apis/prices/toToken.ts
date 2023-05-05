c19622c8ac21529b105263a983ced3d13b00d781
Tothster <itstothster@gmail.com>
2023-05-05 17:01:39 -0300

    prices api

---
+++ apis/prices/toToken.ts
@@ -0,0 +1,12 @@
+import { Token } from '@dozer/currency'
+import { dbTokenWithPools, dbToken } from './interfaces'
+
+export default function toToken(dbToken: dbToken | dbTokenWithPools): Token {
+  return new Token({
+    chainId: dbToken.chainId,
+    uuid: dbToken.uuid,
+    decimals: dbToken.decimals,
+    name: dbToken.name,
+    symbol: dbToken.symbol,
+  })
+}
