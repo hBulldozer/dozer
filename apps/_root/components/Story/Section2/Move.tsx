ecc961bc16ffa62e37c3e82adc9ba2a4461115d5
Tothster <itstothster@gmail.com>
2023-08-08 18:28:56 -0300

    adjust move image

--- apps/_root/components/Story/Section2/Move.tsx
+++ apps/_root/components/Story/Section2/Move.tsx
@@ -7,14 +7,14 @@ import { MoveImage } from './MoveImage'
 export const Move: FC = () => {
   return (
     <section className="py-20 sm:py-40">
-      <Container maxWidth="5xl" className="mx-auto px-4 space-y-20">
+      <Container maxWidth="5xl" className="px-4 mx-auto space-y-20">
         <div className="grid grid-cols-1 lg:grid-cols-[380px_auto] justify-center gap-x-[100px] gap-y-[20px]">
-          <div className="order-2  flex flex-col justify-center gap-3">
+          <div className="flex flex-col justify-center order-2 gap-3">
             <div className="flex flex-col items-center lg:items-start">
               <Typography variant="h1" weight={600} className="text-center lg:text-left">
                 Move assets across networks.
               </Typography>
-              <Typography variant="lg" weight={400} className="text-center lg:text-left mt-2">
+              <Typography variant="lg" weight={400} className="mt-2 text-center lg:text-left">
                 Bring your assets to Hathor Network, enjoy zero fess and blasting speed on your transactions.
               </Typography>
               <div className="flex gap-6">
@@ -31,7 +31,7 @@ export const Move: FC = () => {
               </div>
             </div>
           </div>
-          <div className="order-1  flex justify-center">
+          <div className="flex justify-center order-1 mr-12">
             <MoveImage />
           </div>
         </div>
