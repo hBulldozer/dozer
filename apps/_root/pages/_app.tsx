cb1bd63d117c1939d851360649315d7ef6c8cdac
Tothster <itstothster@gmail.com>
2023-08-15 18:12:36 -0300

    fix root favicon

--- apps/_root/pages/_app.tsx
+++ apps/_root/pages/_app.tsx
@@ -47,12 +47,12 @@ const MyApp: FC<AppProps> = ({ Component, pageProps }) => {
     <>
       <Head>
         <title>Dozer - The future of finance - Coming soon</title>
-        <link rel="apple-touch-icon" sizes="180x180" href="/earn/apple-touch-icon.png?v=1" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/earn/favicon-32x32.png?v=1" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/earn/favicon-16x16.png?v=1" />
-        <link rel="manifest" href="/earn/site.webmanifest?v=1" />
-        <link rel="mask-icon" href="/earn/safari-pinned-tab.svg?v=1" color="#fa52a0" />
-        <link rel="shortcut icon" href="/earn/favicon.ico?v=1" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png?v=1" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png?v=1" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png?v=1" />
+        <link rel="manifest" href="/images/site.webmanifest?v=1" />
+        <link rel="mask-icon" href="/images/safari-pinned-tab.svg?v=1" color="#fa52a0" />
+        <link rel="shortcut icon" href="/images/favicon.ico?v=1" />
       </Head>
       <ThemeProvider>
         <App.Shell>
