389890c9010bd56fe392119bfc4699b46fee8b16
hBulldozer <bulldozer@dozer.finance>
2023-12-28 10:49:54 -0300

    trpc

--- apps/earn/pages/nanostate.tsx
+++ apps/earn/pages/nanostate.tsx
@@ -2,12 +2,28 @@ import { RouterOutputs, api } from '../utils/trpc'
 import { useAccount } from '@dozer/zustand'
 
 const Nanostate = () => {
-  const userAddress = useAccount().address
-  // const { data } = api.getNanoState.byUser.useQuery(userAddress)
-  // const { data } = api.getNanoState.maxWithdrawByUser.useQuery(userAddress)
-  const { data } = api.getNanoState.api.useQuery()
-  console.log(data?.owner_balance)
-  return <div> {data?.rewards_per_share}</div>
+  const userAddress = 'Wcwqb5eX1BYAjazBfHNPaFtp9344aPUv7L'
+  const contractId = '000003248a36eae2438b46164fa410355f235f7a9ed6376e34ea948c09d73f3f'
+
+  const { data: data1 } = api.getNanoState.byUser.useQuery(userAddress)
+  const { data: data2 } = api.getNanoState.maxWithdrawByUser.useQuery(userAddress)
+  const { data: data3 } = api.getNanoState.api.useQuery()
+  const { data: data4 } = api.getProfile.pool.useQuery({ address: userAddress, contractId: contractId })
+
+  console.log(data1, data3, data4) // Log all data for debugging
+
+  return (
+    <div>
+      <h2>Nanostate by User:</h2>
+      <pre>{JSON.stringify(data1, null, 2)}</pre>
+      <h2>Max Withdraw by User:</h2>
+      <pre>{JSON.stringify(data2, null, 2)}</pre>
+      <h2>Nanostate API:</h2>
+      <pre>{JSON.stringify(data3, null, 2)}</pre>
+      <h2>Profile Pool:</h2>
+      <pre>{JSON.stringify(data4, null, 2)}</pre>
+    </div>
+  )
 }
 
 export default Nanostate
--- packages/api/src/router/profile.ts
+++ packages/api/src/router/profile.ts
@@ -22,4 +22,28 @@ export const profileRouter = createTRPCRouter({
       const data = await response.json()
       return data
     }),
+  pool: procedure
+    .input(
+      z.object({
+        address: z.string(),
+        contractId: z.string(),
+      })
+    )
+    .output(
+      z.object({
+        user_deposit_a: z.number(),
+        user_deposit_b: z.number(),
+        rewards_user_dzr: z.number(),
+      })
+    )
+    .query(async ({ input }) => {
+      const apiUrl = `http://localhost:8080/v1a/nano_contract/state`
+      const queryParams = [`id=${input.contractId}`, `calls[]=front_end_api_user("a'${input.address}'")`]
+      const fetchUrl = `${apiUrl}?${queryParams.join('&')}`
+
+      const response = await fetch(fetchUrl)
+      const data = await response.json()
+      const result = data['calls'][`front_end_api_user("a'${input.address}'")`]['value']
+      return result
+    }),
 })
