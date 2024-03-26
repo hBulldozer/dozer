5d406091f187a4ce804e006a1f7c731b0438f074
Bulldozer <bulldozer@dozer.finance>
2024-03-26 18:11:20 -0300

    updated donation to cold wallet addy

--- apps/_root/components/DonateModal.tsx
+++ apps/_root/components/DonateModal.tsx
@@ -1,6 +1,7 @@
 import { App, AppType, Button, CopyHelper, Dialog, IconButton, Link, Menu, Typography } from '@dozer/ui'
 import React, { FC } from 'react'
 import qrcode from '../public/qrcode.jpeg'
+import qrcold from '../public/qrcold.png'
 import Image from 'next/image'
 import { DuplicateIcon } from '@heroicons/react/solid'
 
@@ -9,7 +10,8 @@ interface DonateModalInterface {
   setOpen(open: boolean): void
 }
 export const DonateModal: FC<DonateModalInterface> = ({ open, setOpen }) => {
-  const address = 'HUnmVsAN4tC6geCNe54KniPNbbwkV6kC1e'
+  const address_hot = 'HUnmVsAN4tC6geCNe54KniPNbbwkV6kC1e'
+  const address_cold = 'HJpA7CTpWqL2p2HRtGkGXjbvAzr3sqi1GP'
   return (
     <Dialog open={open} onClose={() => setOpen(false)}>
       <Dialog.Content className="max-w-sm !pb-4">
@@ -18,17 +20,17 @@ export const DonateModal: FC<DonateModalInterface> = ({ open, setOpen }) => {
           <div className="relative flex flex-col col-span-12 my-4 gap-1 p-2 border sm:p-4 rounded-2xl bg-stone-700/40 border-stone-200/5">
             <div className="flex items-center gap-2">
               <div className="flex items-center justify-between w-full gap-2">
-                <Image className="rounded-2xl" alt="qrcode" src={qrcode} />
+                <Image className="rounded-2xl" alt="qrcode" src={qrcold} />
               </div>
             </div>
           </div>
           <div className="relative flex flex-col col-span-12 gap-1 p-3 mt-3 border sm:p-4 rounded-2xl bg-yellow-400 border-yellow-400">
-            <CopyHelper toCopy={address}>
+            <CopyHelper toCopy={address_cold}>
               {(isCopied) => (
                 <IconButton className="p-1" altColor description={isCopied ? 'Copied!' : 'Copy'}>
                   <div className="grid items-center grid-cols-10 gap-1">
                     <DuplicateIcon width={25} height={25} color="black" />
-                    <Typography className="col-span-2 md:text-sm text-xs text-black">{address}</Typography>
+                    <Typography className="col-span-2 md:text-sm text-xs text-black">{address_cold}</Typography>
                   </div>
                 </IconButton>
               )}
