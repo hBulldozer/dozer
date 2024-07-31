a7caf378fceff1184265f6b994616d654e180ac5
tothster <tothster@dozer.finance>
2024-07-31 16:16:58 -0300

    fix: comment out unused components and clean up rendering logic in PairAPRCell, PairNameCell, and PositionQuickHoverTooltip

--- apps/earn/components/PoolsSection/Tables/PositionsTable/Cells/PairNameCell.tsx
+++ apps/earn/components/PoolsSection/Tables/PositionsTable/Cells/PairNameCell.tsx
@@ -23,9 +23,9 @@ export const PairNameCell: FC<CellProps> = ({ row }) => {
       <div className="flex flex-col">
         <Typography variant="sm" weight={500} className="flex items-center gap-1 text-slate-50">
           {token0.symbol} <span className="text-slate-500">/</span> {token1.symbol}{' '}
-          <div className={classNames('bg-slate-700 rounded-lg px-1 py-0.5 ml-1')}>
+          {/* <div className={classNames('bg-slate-700 rounded-lg px-1 py-0.5 ml-1')}>
             {formatNumber(row.feeUSD / 100)}%
-          </div>
+          </div> */}
         </Typography>
         {/* <Typography variant="xxs" className="text-slate-400">
           {row.type === 'STABLE_POOL' && 'Stable'}
