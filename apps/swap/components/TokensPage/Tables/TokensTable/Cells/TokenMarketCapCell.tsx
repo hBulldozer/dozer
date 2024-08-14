05498ba1ebdbd3844c741d61b029c70e89f17c2b
Tothster <itstothster@gmail.com>
2024-08-13 22:55:43 -0300

    fix: loading error on token market cap cell

--- apps/swap/components/TokensPage/Tables/TokensTable/Cells/TokenMarketCapCell.tsx
+++ apps/swap/components/TokensPage/Tables/TokensTable/Cells/TokenMarketCapCell.tsx
@@ -3,18 +3,20 @@ import { Skeleton, Typography } from '@dozer/ui'
 import { FC } from 'react'
 
 import { CellProps } from './types'
-import { hathorLib } from '@dozer/nanocontracts'
 import { api } from 'utils/api'
 
 export const TokenMarketCapCell: FC<CellProps> = ({ row }) => {
   const { data: allTotalSupply, isLoading } = api.getTokens.allTotalSupply.useQuery()
-  const totalSupply = allTotalSupply
-    ? row.id.includes('native')
-      ? allTotalSupply[row.token0.uuid]
-      : allTotalSupply[row.token1.uuid]
-    : 0
   const { data: prices, isLoading: isLoadingPrices } = api.getPrices.all.useQuery()
-  const price = prices ? (row.id.includes('native') ? prices[row.token0.uuid] : prices[row.token1.uuid]) : 0
+
+  if (!allTotalSupply || !prices)
+    return (
+      <div className="flex flex-col gap-1 justify-center flex-grow h-[44px]">
+        <Skeleton.Box className="ml-4 h-[22px] bg-white/[0.06] rounded-full" />
+      </div>
+    )
+  const totalSupply = row.id.includes('native') ? allTotalSupply[row.token0.uuid] : allTotalSupply[row.token1.uuid]
+  const price = row.id.includes('native') ? prices[row.token0.uuid] : prices[row.token1.uuid]
 
   return isLoading || isLoadingPrices ? (
     <div className="flex flex-col gap-1 justify-center flex-grow h-[44px]">
