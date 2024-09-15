a182be779d3d0823c380bf48ea3803a116b5f8ba
Bulldozer <bulldozer@dozer.finance>
2024-09-15 15:02:35 -0300

    deploying

--- apps/earn/components/PoolsSection/Tables/PoolsTable/PoolsTable.tsx
+++ apps/earn/components/PoolsSection/Tables/PoolsTable/PoolsTable.tsx
@@ -241,7 +241,6 @@ export const PoolsTable: FC = () => {
     // [sorting, pagination, selectedNetworks, selectedPoolTypes, farmsOnly, query, extraQuery]
   )
 
-  // console.log({ pools })
 
   const table = useReactTable<ExtendedPair>({
     data: pools || [],
