542711434843e73599b20888036ba625bc82288b
Bulldozer <bulldozer@dozer.finance>
2024-09-15 15:58:31 -0300

    fix: refine price formatting logic for values less than 1 to improve currency display precision

--- packages/format/src/price.ts
+++ packages/format/src/price.ts
@@ -2,11 +2,11 @@ import numeral from 'numeral'
 
 export const formatUSD = (value: string | number, inputString = '$0.00a') => {
   return numeral(value).format(
-    Number(value) < 1 && Number(value) != 0
+    Number(value) < 1 && Number(value) > 0.1 && Number(value) != 0
       ? '$0.000a'
-      : Number(value) < 0.1 && Number(value) != 0
+      : Number(value) < 0.1 && Number(value) > 0.01 && Number(value) != 0
       ? '$0.0000a'
-      : Number(value) < 0.01 && Number(value) != 0
+      : Number(value) < 0.01 && Number(value) > 0.001 && Number(value) != 0
       ? '$0.00000a'
       : Number(value) < 0.001 && Number(value) != 0
       ? '$0.000000a'
