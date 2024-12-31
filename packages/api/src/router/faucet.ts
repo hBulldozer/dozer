dfd4f0c2dd8b52f6ea929da2e8052e0bc1730962
tothster <tothster@dozer.finance>
2024-12-30 21:40:38 -0300

    fix: more debugs on faucet

--- packages/api/src/router/faucet.ts
+++ packages/api/src/router/faucet.ts
@@ -16,52 +16,56 @@ export const faucetRouter = createTRPCRouter({
         }
     }
     console.log(`Trying to start wallet`)
-    const start = await fetch(`${process.env.LOCAL_WALLET_MASTER_URL}/start`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'X-API-Key': process.env.WALLET_API_KEY || '',
-      },
-      body: JSON.stringify({ 'wallet-id': process.env.WALLET_ID, seedKey: 'genesis' }),
-    })
-    console.log(`Started wallet. Sending TX`)
-    const response = await fetch(`${process.env.LOCAL_WALLET_MASTER_URL}/wallet/simple-send-tx`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'x-wallet-id': process.env.WALLET_ID || '',
-        'x-api-key': process.env.WALLET_API_KEY || '',
-      },
-      body: JSON.stringify({
-        address: input.address,
-        value: 5_000_00,
-        token: '00',
-      }),
-    })
-    console.log(response)
+    try {
+      const start = await fetch(`${process.env.LOCAL_WALLET_MASTER_URL}/start`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'X-API-Key': process.env.WALLET_API_KEY || '',
+        },
+        body: JSON.stringify({ 'wallet-id': process.env.WALLET_ID, seedKey: 'genesis' }),
+      })
+      console.log(`Started wallet. Sending TX`)
+      const response = await fetch(`${process.env.LOCAL_WALLET_MASTER_URL}/wallet/simple-send-tx`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'x-wallet-id': process.env.WALLET_ID || '',
+          'x-api-key': process.env.WALLET_API_KEY || '',
+        },
+        body: JSON.stringify({
+          address: input.address,
+          value: 5_000_00,
+          token: '00',
+        }),
+      })
+      console.log(response)
 
-    const data = await response.json()
+      const data = await response.json()
 
-    console.log(data)
+      console.log(data)
 
-    if (!data || !data.hash) {
-      throw new Error('Failed to create token on blockchain')
+      if (!data || !data.hash) {
+        throw new Error('Failed to create token on blockchain')
+      }
+
+      const database_save = await ctx.prisma.faucet.create({
+        data: {
+          address: input.address,
+          amount: 5_000_00,
+          date: new Date(),
+          hash: data.hash,
+        },
+      })
+
+      if (!database_save) {
+        throw new Error('Failed to create token on blockchain')
+      }
+      return { success: true, message: 'Faucet transaction created', hash: data.hash }
+    } catch (e) {
+      console.log(e)
+      return { success: false, message: 'Failed to send HTR' }
     }
-
-    const database_save = await ctx.prisma.faucet.create({
-      data: {
-        address: input.address,
-        amount: 5_000_00,
-        date: new Date(),
-        hash: data.hash,
-      },
-    })
-
-    if (!database_save) {
-      throw new Error('Failed to create token on blockchain')
-    }
-
-    return { success: true, message: 'Faucet transaction created', hash: data.hash }
   }),
   checkFaucet: procedure.input(z.object({ address: z.string() })).query(async ({ ctx, input }) => {
     const transactions = await ctx.prisma.faucet.findMany({
