c6ad3399f5efd89baac080589bbfdd970fcee9ab
tothster <tothster@dozer.finance>
2025-02-10 11:45:25 -0300

    fiz: z-index on mobile for profiel panel

--- packages/higmi/components/Wallet/Profile/Profile.tsx
+++ packages/higmi/components/Wallet/Profile/Profile.tsx
@@ -86,7 +86,7 @@ export const Profile: FC<ProfileProps> = ({ client }) => {
 
   if (address) {
     const panel = (
-      <Popover.Panel className="w-full sm:w-[320px] fixed bottom-0 left-0 right-0 sm:absolute sm:bottom-[unset] sm:left-[unset] mt-4 sm:rounded-xl rounded-b-none shadow-md shadow-black/[0.3] bg-stone-900 border border-stone-200/20">
+      <Popover.Panel className="z-[99] w-full sm:w-[320px] fixed bottom-0 left-0 right-0 sm:absolute sm:bottom-[unset] sm:left-[unset] mt-4 sm:rounded-xl rounded-b-none shadow-md shadow-black/[0.3] bg-stone-900 border border-stone-200/20">
         {view === ProfileView.Default && (
           <Default api_client={client} chainId={chainId} address={address} setView={setView} />
         )}
