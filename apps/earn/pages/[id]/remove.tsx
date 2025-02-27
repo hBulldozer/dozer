7fd83b24a9ee2e1770494d7745dca99962a2d281
tothster <tothster@dozer.finance>
2025-02-27 15:52:16 -0300

    fix: remove liquidity component

--- apps/earn/pages/[id]/remove.tsx
+++ apps/earn/pages/[id]/remove.tsx
@@ -39,7 +39,7 @@ const Remove: NextPage = () => {
   if (!pair) return <></>
   const tokens = pair ? [pair.token0, pair.token1] : []
   if (!tokens) return <></>
-  const { data: prices = {} } = api.getPrices.firstLoadAll.useQuery()
+  const { data: prices = {} } = api.getPrices.all.useQuery()
   if (!prices) return <></>
 
   return (
