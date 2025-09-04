604d4d1a8561f70b636efb5713182ebb46c0e113
hBulldozer <bulldozer@dozer.finance>
2025-09-03 23:24:48 -0300

    fix: update copyright year in Footer component

--- packages/ui/app/Footer.tsx
+++ packages/ui/app/Footer.tsx
@@ -149,7 +149,7 @@ export function Footer(props: FooterProps): JSX.Element {
       <Container maxWidth="5xl" className="mx-auto mt-8 mb-2">
         <div className="flex justify-between py-2 mx-4 border-t border-stone-800">
           <Typography variant="xs" className="text-stone-300">
-            Copyright © 2024 Dozer. All rights reserved.
+            Copyright © 2025 Dozer. All rights reserved.
           </Typography>
           <div className="flex divide-x divide-stone-100/20 gap-">
             <Link.Internal href="https://mvp.dozer.finance" passHref={true}>
